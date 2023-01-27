b5d4a2c514b62159ad47bfc291efc35ace4997bf
aniebietafia <aniebietafia87@gmail.com>
2023-01-27 08:25:37 +0100

    Updating the root file with ejs feature

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ const app = express();
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// Setting up the ejs view engine
+app.set("view engine", "view");
+app.set("views", "views");
+
 const PORT = process.env.PORT || 4000;
 
 // Server listening for incoming requests
